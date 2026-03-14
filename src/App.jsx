154d8839d0f49a2d346197044f6de830cf08708f
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-03-14 22:24:31 +0600

    react world on the go

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Countries from './Components/Countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+     
+          <h2>React World On The Go</h2>
+        <Countries></Countries>
+          
+    </>
+  )
+}
+
+export default App
