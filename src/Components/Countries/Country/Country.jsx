cec8a372ca9fc132b332784c553a16a9c69bc40d
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-03-15 22:30:13 +0600

    set condition in btn

--- src/Components/Countries/Country/Country.jsx
+++ src/Components/Countries/Country/Country.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
+import './country-visited.css'
 const Country = ({country}) => {
 //   console.log(country.population.population);
+
+const [visited, setVisited]=useState(false)
 const handleVisited=()=>{
-    console.log('BTN Clicked')
+    // console.log('BTN Clicked')
+    if(visited==true){
+        setVisited(false)
+    }
+    else{
+        setVisited(true)
+    }
 }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name:{country.name.common}</h3>
             <p>Population:{country.population.population}</p>
             <p>Area:{country.area.area} {country.area.area>300000?"Big Country":"Small Country"}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button onClick={handleVisited}>{visited?'Visited':'not visited'}</button>
         </div>
     );
 };
