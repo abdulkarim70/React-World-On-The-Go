b0d608306f3f48acc12479ab76c2feb08558f587
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-03-15 17:22:42 +0600

    Flag image shown

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { use } from 'react';
+import Country from './Country/Country';
+import './Countries.css'
 const Countries = ({countriesPromise}) => {
     const countriesData=use(countriesPromise)
   
     const countries=countriesData.countries
-    console.log(countries);
+ 
     return (
-        <div>
-          <h1>In the countries:{countries.length}</h1>  
+        <div >
+          <h2>In the countries:{countries.length}</h2>  
+          <div style={{display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gap: '20px'}} >
+        {
+            countries.map(country=><Country key={country.cca3.cca3} country={country}></Country>)
+          }
+    </div>
+          
         </div>
     );
 };
